2eeb2fcf66ee8fa40e2b7a1cd77aacb31d8d8f42
melennaya <tatiana.melennaya@gmail.com>
2014-11-25 09:30:15 +0200

    добавлены страницы: exchange-ads, exchange-buy,  finance-balance

---
+++ blocks/pages/exchange-buy.php
@@ -0,0 +1,228 @@
+<div class="wrapper">
+	<div class="exchange exchange-buy">
+
+		<div class="title title--exchange-ads">Параметры поиска:</div>
+			<div class="exchange-buy__search-options">
+				<div class="container container--left">
+					<div class="option-line">
+						<span class="option-name">Подписки:</span>
+						<div class="filter__date filter__date--exchange-buy">
+							<span class="filter__date--title exchange-buy__filter--date-title">с</span>
+							<input type="date" name="prev-date" class="date date--prev exchange-buy__date--prev" />
+							<span class="filter__date--title exchange-buy__filter--date-title">до</span>
+							<input type="date" name="next-date" class="date date--next" />
+						</div>
+					</div>
+					<div class="option-line">
+						<span class="option-name">Ребилы:</span>
+						<div class="filter__date filter__date--exchange-buy">
+							<span class="filter__date--title exchange-buy__filter--date-title">с</span>
+							<input type="date" name="prev-date" class="date date--prev exchange-buy__date--prev" />
+							<span class="filter__date--title exchange-buy__filter--date-title">до</span>
+							<input type="date" name="next-date" class="date date--next" />
+						</div>
+					</div>
+					<div class="option-line">
+						<span class="option-name">Профит:</span>
+						<div class="filter__date filter__date--exchange-buy">
+							<span class="filter__date--title exchange-buy__filter--date-title">от</span>
+							<input type="text" name="" class="input-box input-box--prev exchange-buy__input-box--prev" />
+							<span class="filter__date--title exchange-buy__filter--date-title">до</span>
+							<input type="text" name="" class="input-box input-box--next exchange-buy__input-box--next" />
+						</div>
+					</div>
+					<div class="option-line">
+						<span class="option-name">Кол-во ребилов:</span>
+						<div class="filter__date filter__date--exchange-buy">
+							<span class="filter__date--title exchange-buy__filter--date-title">от</span>
+							<input type="text" name="" class="input-box input-box--prev exchange-buy__input-box--prev" />
+							<span class="filter__date--title exchange-buy__filter--date-title">до</span>
+							<input type="text" name="" class="input-box input-box--next exchange-buy__input-box--next" />
+						</div>
+					</div>
+					<div class="option-line">
+						<span class="option-name">Цена подписки:</span>
+						<div class="filter__date filter__date--exchange-buy">
+							<span class="filter__date--title exchange-buy__filter--date-title">от</span>
+							<input type="text" name="" class="input-box input-box--prev exchange-buy__input-box--prev" />
+							<span class="filter__date--title exchange-buy__filter--date-title">до</span>
+							<input type="text" name="" class="input-box input-box--next exchange-buy__input-box--next" />
+						</div>
+					</div>
+				</div>
+
+				<div class="container container--left">
+					<span>Ваш баланс:</span>
+				</div>
+			</div>
+		<div class="buy-selected">
+			<form>
+				<input class="btn btn--active" type="submit" value="Купить выбранные">
+
+				<div class="filter filter--exchange-buy">
+					<div class="filter__date filter__date--exchange-buy">
+						<span class="filter__date--title">Период:</span>
+						<input type="date" name="prev-date" class="date date--prev" /> &mdash;
+						<input type="date" name="next-date" class="date date--next" />
+					</div>
+
+					<div class="filter__choice--exchange-buy">
+						<span class="filter__choice--title">Предложений на странице:</span>
+						<select name="last" class="select select--choice">
+							<option value="">10</option>
+						</select>
+					</div>
+				</div>
+
+				<div class="table">
+					<div class="table__row table__row--header">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell">№</div>
+						<div class="table__row--cell">Продавец</div>
+						<div class="table__row--cell">Дата размещения</div>
+						<div class="table__row--cell">Биллинги</div>
+						<div class="table__row--cell">Тематика</div>
+						<div class="table__row--cell">Операторы</div>
+						<div class="table__row--cell">Подписки (шт.)</div>
+						<div class="table__row--cell">Ребилы (шт.)</div>
+						<div class="table__row--cell">Профит (шт.)</div>
+						<div class="table__row--cell">Средний ребил (руб.)</div>
+						<div class="table__row--cell">Цена подписки (руб.)</div>
+						<div class="table__row--cell">Цена пакета (руб.)</div>
+					</div>
+
+					<div class="table__row table__row--tbody">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell"><a href="#" class="dashed-link dashed-link--balance">15315</a></div>
+						<div class="table__row--cell">mozg</div>
+						<div class="table__row--cell">12-09-2014</div>
+						<div class="table__row--cell">Борнпей</div>
+						<div class="table__row--cell">WAP</div>
+						<div class="table__row--cell">megafon, mts</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">5</div>
+						<div class="table__row--cell">3</div>
+						<div class="table__row--cell">30</div>
+						<div class="table__row--cell">60</div>
+					</div>
+
+					<div class="table__row table__row--tbody">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell"><a href="#" class="dashed-link dashed-link--balance">15315</a></div>
+						<div class="table__row--cell">mozg</div>
+						<div class="table__row--cell">12-09-2014</div>
+						<div class="table__row--cell">Борнпей</div>
+						<div class="table__row--cell">WAP</div>
+						<div class="table__row--cell">megafon, mts</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">5</div>
+						<div class="table__row--cell">3</div>
+						<div class="table__row--cell">30</div>
+						<div class="table__row--cell">60</div>
+					</div>
+
+					<div class="table__row table__row--tbody">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell"><a href="#" class="dashed-link dashed-link--balance">15315</a></div>
+						<div class="table__row--cell">mozg</div>
+						<div class="table__row--cell">12-09-2014</div>
+						<div class="table__row--cell">Борнпей</div>
+						<div class="table__row--cell">WAP</div>
+						<div class="table__row--cell">megafon, mts</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">5</div>
+						<div class="table__row--cell">3</div>
+						<div class="table__row--cell">30</div>
+						<div class="table__row--cell">60</div>
+					</div>
+
+					<div class="table__row table__row--tbody">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell"><a href="#" class="dashed-link dashed-link--balance">15315</a></div>
+						<div class="table__row--cell">mozg</div>
+						<div class="table__row--cell">12-09-2014</div>
+						<div class="table__row--cell">Борнпей</div>
+						<div class="table__row--cell">WAP</div>
+						<div class="table__row--cell">megafon, mts</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">5</div>
+						<div class="table__row--cell">3</div>
+						<div class="table__row--cell">30</div>
+						<div class="table__row--cell">60</div>
+					</div>
+
+					<div class="table__row table__row--tbody">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell"><a href="#" class="dashed-link dashed-link--balance">15315</a></div>
+						<div class="table__row--cell">mozg</div>
+						<div class="table__row--cell">12-09-2014</div>
+						<div class="table__row--cell">Борнпей</div>
+						<div class="table__row--cell">WAP</div>
+						<div class="table__row--cell">megafon, mts</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">5</div>
+						<div class="table__row--cell">3</div>
+						<div class="table__row--cell">30</div>
+						<div class="table__row--cell">60</div>
+					</div>
+
+					<div class="table__row table__row--tbody">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell"><a href="#" class="dashed-link dashed-link--balance">15315</a></div>
+						<div class="table__row--cell">mozg</div>
+						<div class="table__row--cell">12-09-2014</div>
+						<div class="table__row--cell">Борнпей</div>
+						<div class="table__row--cell">WAP</div>
+						<div class="table__row--cell">megafon, mts</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">5</div>
+						<div class="table__row--cell">3</div>
+						<div class="table__row--cell">30</div>
+						<div class="table__row--cell">60</div>
+					</div>
+
+					<div class="table__row table__row--tbody">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell"><a href="#" class="dashed-link dashed-link--balance">15315</a></div>
+						<div class="table__row--cell">mozg</div>
+						<div class="table__row--cell">12-09-2014</div>
+						<div class="table__row--cell">Борнпей</div>
+						<div class="table__row--cell">WAP</div>
+						<div class="table__row--cell">megafon, mts</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">5</div>
+						<div class="table__row--cell">3</div>
+						<div class="table__row--cell">30</div>
+						<div class="table__row--cell">60</div>
+					</div>
+
+					<div class="table__row table__row--tbody">
+						<div class="table__row--cell"><input type="checkbox"></div>
+						<div class="table__row--cell"><a href="#" class="dashed-link dashed-link--balance">15315</a></div>
+						<div class="table__row--cell">mozg</div>
+						<div class="table__row--cell">12-09-2014</div>
+						<div class="table__row--cell">Борнпей</div>
+						<div class="table__row--cell">WAP</div>
+						<div class="table__row--cell">megafon, mts</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">2</div>
+						<div class="table__row--cell">5</div>
+						<div class="table__row--cell">3</div>
+						<div class="table__row--cell">30</div>
+						<div class="table__row--cell">60</div>
+					</div>
+
+				</div>
+				<!-- /table -->
+			</form>
+	</div>
+	<!-- /exchange exchange-ads -->
+</div>
+<!-- /wrapper -->
---
+++ blocks/pages/finance-balance.php
@@ -0,0 +1,192 @@
+<div class="wrapper">
+	<div class="finance finance-balance">
+
+		<div class="container container--left">
+			<div class="title">История операций</div>
+			<div class="filter">
+				<div class="filter__date filter__date--finance">
+					<span class="filter__date--title">Период:</span>
+					<input type="date" name="prev-date" class="date date--prev" /> &mdash;
+					<input type="date" name="next-date" class="date date--next" />
+				</div>
+
+				<div class="filter__choice--finance">
+					<span class="filter__choice--title">Показать последние:</span>
+					<select name="last" class="select select--choice">
+						<option value="">4</option>
+					</select>
+				</div>
+			</div>
+			<!-- /filter -->
+
+			<div class="table table-stat">
+				<div class="table__row table__row--header table-stat__row">
+					<div class="table__row--cell table-stat__row--cell">Дата/Время</div>
+					<div class="table__row--cell table-stat__row--cell">Сумма (руб.)</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Операция</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--tbody table-stat__row--tbody">
+					<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+					<div class="table__row--cell table-stat__row--cell">73,15</div>
+					<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+				</div>
+
+				<div class="table__row table__row--footer table-stat__row--footer">
+					<div class="table__row--cell table-stat__row--cell">Всего</div>
+					<div class="table__row--cell table-stat__row--cell"></div>
+					<div class="table__row--cell table-stat__row--cell  table-stat__row--last"></div>
+				</div>
+			</div>
+			<!-- /table -->
+		</div>
+
+		<div class="container container--right">
+			<div class="balance">
+				<span class="balance__title">Ваш баланс:</span>
+				<div class="balance__rub">
+					<div class="group">
+						<span class="money money--bag">20 394</span><span class="valuta">руб</span>
+					</div>
+					<a href="#" class="dashed-link dashed-link--balance">Пополнить счет</a>
+					<a href="#" class="btn btn--active btn--balance-rub">Заказать досрочную выплату</a>
+
+					<span class="balance__title balance__title--rub">Кошелёк для выплат:</span>
+					<span class="pole pole--balance">R188258104531</span>
+					<a href="#" class="btn btn--active">Изменить</a>
+				</div>
+				<div class="balance__limons">
+					<div class="group">
+						<span class="money money--lemon">3874</span><span class="valuta">лимонов</span>
+					</div>
+					<a href="#" class="dashed-link dashed-link--balance">Условия</a>
+					<span href="#" class="narrow">Минимальная сумма заказа досрочной выплаты - 100р.</span>
+
+					<span class="balance__title balance__title--limons">Автоматические выплаты:</span>
+					<a href="#" class="btn btn--active">Включено</a>
+					<a href="#" class="btn">Отключить</a>
+				</div>
+			</div>
+
+			<div class="automatically-purchased">
+				<div class="title">Автоматически купленные подписки</div>
+				<div class="filter">
+					<div class="filter__date filter__date--finance">
+						<span class="filter__date--title">Период:</span>
+						<input type="date" name="prev-date" class="date date--prev" /> &mdash;
+						<input type="date" name="next-date" class="date date--next" />
+					</div>
+
+					<div class="filter__choice--finance">
+						<span class="filter__choice--title">Показать последние:</span>
+						<select name="last" class="select select--choice">
+							<option value="">4</option>
+						</select>
+					</div>
+				</div>
+
+				<div class="table table-stat">
+					<div class="table__row table__row--header table-stat__row">
+						<div class="table__row--cell table-stat__row--cell">Дата/Время</div>
+						<div class="table__row--cell table-stat__row--cell">Сумма (руб.)</div>
+						<div class="table__row--cell table-stat__row--cell table-stat__row--last">Операция</div>
+					</div>
+
+					<div class="table__row table__row--tbody table-stat__row--tbody">
+						<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+						<div class="table__row--cell table-stat__row--cell">73,15</div>
+						<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+					</div>
+
+					<div class="table__row table__row--tbody table-stat__row--tbody">
+						<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+						<div class="table__row--cell table-stat__row--cell">73,15</div>
+						<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+					</div>
+
+					<div class="table__row table__row--tbody table-stat__row--tbody">
+						<div class="table__row--cell table-stat__row--cell">13.08.2014</div>
+						<div class="table__row--cell table-stat__row--cell">73,15</div>
+						<div class="table__row--cell table-stat__row--cell table-stat__row--last">Выплата</div>
+					</div>
+
+					<div class="table__row table__row--footer table-stat__row--footer">
+						<div class="table__row--cell table-stat__row--cell">Всего</div>
+						<div class="table__row--cell table-stat__row--cell"></div>
+						<div class="table__row--cell table-stat__row--cell  table-stat__row--last"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+
+	</div>
+	<!-- /finanse finanse-balance -->
+</div>
+<!-- /wrapper -->
